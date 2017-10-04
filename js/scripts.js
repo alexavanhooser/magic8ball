257bee09a02476b897cbf4731057397d117f26c0
Alexa VanHooser <alexavanhooser@gmail.com>
2017-10-03 17:38:52 -0700

    made 8 ball functional

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,17 @@
 // target your button and assign an ask function that outputs a random image to your page
 var random = Math.floor(Math.random() * 21);
 var eightBall = '';
-  switch( Math.floor(Math.random() * 21)) {
+  //switch( Math.floor(Math.random() * 21)) {
+  let userQuestion = "";
     document.getElementById("button").addEventListener("click", function(){
-       document.body.style.backgroundImage = "url('../img/magic8ball_"+random+".png')";
-  //  document.getElementById('button');
-    eightBall =  document.body.style.backgroundImage = "url(../img/magic8ball_1.png)";
+       document.getElementById("magic-8").style.backgroundImage = "url('img/magic8ball_"+random+".png')";
+userQuestion = document.getElementsByClassName('question').value;
+    //eightBall =  document.body.style.backgroundImage = "url(img/magic8ball_1.png)";
     //'It is certain';
   //  break;
   //  eightBall = document.body.style.backgroundImage = "url(../img/magic8ball_2.png)";
   //  break;
-}
+});
 
 console.log('You asked: ' + userQuestion);
 console.log('The response was: ' + eightBall);
